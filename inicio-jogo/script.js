a5b96300bbf42416c66b931ea81fbd23cf687ce6
Marcos Vinicius de Araujo Silva <e_marcossilva@jfrn.jus.br>
2023-11-29 12:15:39 -0300

    criação do da máquina de escrever

--- inicio-jogo/script.js
+++ inicio-jogo/script.js
@@ -1,24 +1,52 @@
-var frase = "bem-vindo ao game!"
+var fra = [
+  "Bem-vindo ao jogo!",
+  "Esta é a segunda frase.",
+  "Adicione quantas frases desejar."
+];
 
+/* var fra = [
+  "Welcome the game!",
+  "This is the second phrase.",
+  "Add how many phrases you wish."
+];
+ */
 async function typeWriter(text, speed) {
-    const outputElement = document.getElementsByTagName('p')[0];
-    for (let i = 0; i < text.length; i++) {
-      outputElement.textContent += text[i];
-      await sleep(speed);
-    }
+  const outputElement = document.getElementsByTagName('p')[0];
+  for (let i = 0; i < text.length; i++) {
+    outputElement.innerHTML += text[i];
+    await sleep(speed);
   }
+  outputElement.innerHTML += '<br>'; // Adiciona uma quebra de linha ao final de cada frase
+}
 
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function runTypewriter(frases) {
+  for (let i = 0; i < frases.length; i++) {
+    await typeWriter(frases[i], 50);
   }
+  creatBottom()
+}
 
-  async function runTypewriter() {
-    await typeWriter(frase, 50);
-    // Adicione mais chamadas a typeWriter conforme necessário
 
-    // Exemplo adicional:
-    // await sleep(1000); // Espera por 1 segundo
-    // await typeWriter("Outra parte da resposta.", 50);
-  }
+function creatBottom(){
+const button = document.createElement('button');
 
-  runTypewriter();
+// Define o texto do botão
+button.textContent = 'Clique em mim';
+
+// Adiciona um manipulador de eventos ao botão (opcional)
+button.addEventListener('click', function() {
+  alert('Botão clicado!');
+});
+
+// Encontra o elemento no qual você deseja anexar o botão (por exemplo, o corpo do documento)
+const body = document.getElementById('bottom');
+
+// Anexa o botão ao elemento desejado
+body.appendChild(button);
+}
+
+runTypewriter(fra);
